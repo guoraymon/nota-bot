666ae0aac14b2ebc467f5cb63daa370fca676e33
guoraymon <guoraymon@gmail.com>
2026-09-02 10:02:17 +0800

    refactor(bot): drop elapsed seconds from reply stats footer

--- src/main.rs
+++ src/main.rs
@@ -268,8 +268,7 @@ async fn main() {
 
             if let Some(last_content) = last_content {
                 let content = format!(
-                    "{last_content}\n\n> {}s {:.1} tok/s ↑{prompt_tokens} ↓{completion_tokens} CH{:.2}%",
-                    total_elapsed.as_secs(),
+                    "{last_content}\n\n> {:.1} tok/s ↑{prompt_tokens} ↓{completion_tokens} CH{:.2}%",
                     if total_elapsed.as_secs_f64() > 0.0 {
                         completion_tokens as f64 / total_elapsed.as_secs_f64()
                     } else {
